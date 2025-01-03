6af0e68c3c67007e17162e1b83a81d4fc4d0b4c3
shiboka <d.aubie@outlook.com>
2025-01-03 14:57:58 -0400

    https fix i hope

--- sneed_server/app.py
+++ sneed_server/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, current_app
+from flask import Flask, current_app, redirect, request
 
 app = Flask(__name__, static_url_path='')
+
+@app.before_request
+def enforce_https():
+    if request.headers.get('X-Forwarded-Proto') == 'http':
+        return redirect(request.url.replace('http://', 'https://'), code=301)
 
 @app.route('/')
 def app_home():  # put application's code here
